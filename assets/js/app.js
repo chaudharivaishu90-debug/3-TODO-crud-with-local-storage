c2f93ffbc86aad4bda74d7585dc17b632ffd50c0
chaudharivaishu90-debug <chaudharivaishu90@gmail.com>
2026-09-05 17:08:09 +0530

    TODO with LS4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,61 @@
-var cl=console.log;
+var cl = console.log;
 
-const form=document.getElementById("form");
-const formInput1=document.getElementById("formInput1");
-const formInput2=document.getElementById("formInput2");
 const list=document.getElementById("list");
-const submitButton=document.getElementById("submitButton")
- const updateButton=document.getElementById("updateButton");
+const form=document.getElementById("form");
+const Study=document.getElementById("Study");
+const Topic=document.getElementById("Topic");
+const UpdateBtn=document.getElementById("UpdateBtn");
+const SubmitBtn=document.getElementById("SubmitBtn");
+const Date=document.getElementById("Date");
 
-// let array1=[{id:"1",name:"vaishali",dob:"01/04/1998"}];
-// localStorage.setItem("array1",JSON.stringify(array1));
 
-let array1=JSON.parse(localStorage.getItem("array1"))||[];
-cl(array1);
-function ShowData(eve) {
+
+// let WorkArr=[{id:"1",Task:"Study",date:"01/02/2026",topic:"Angular"}];
+// localStorage.setItem("WorkArr",JSON.stringify(WorkArr));
+let WorkArr =JSON.parse(localStorage.getItem("WorkArr"))||[];
+
+function ShowLi(array) {
     let result=``;
-    eve.forEach((ele) =>{
-        result+=` <li class="list-group-item d-flex justify-content-between align-items-center"id="${ele.id}">
-                            <strong>${ele.name}</strong> 
-                             <strong>${ele.dob}</strong>
-                            <div>  
-                                <i onclick="OnEdit(this)" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
-                                <i onclick="OnDlt(this)" class="fa-solid fa-trash fa-2x fa-2x text-danger"></i> 
-                       
-                             </div> 
-                         </li>
-
+    array.forEach(ele => {
+        result+=`                <li class="card mb-3 p-3" id="${ele.id}">
+                    <div>
+                    <strong >${ele.Task}</strong>
+                    </div>
+                    <span>${ele.date}</span>
+                    <p>${ele.topic}</p>
+                    <div class="d-flex justify-content-between">
+                    <i onclick ="OnEdt(this)" class="fa-sharp fa-solid fa-pen-to-square fa-2x text-primary"></i>
+                    <i onclick ="OnRemove(this)" class="fa-sharp fa-solid fa-trash fa-2x text-danger"></i>
+                    </div>
+                </li>
         `
-        
     });
-    list.innerHTML=result;
+list.innerHTML=result;
+    
 }
-ShowData(array1);
+ShowLi(WorkArr);
 
 
-
-//Read
-function createLI(eve){
+//create
+function createLI(obj){
     let li=document.createElement("li");
-    li.id=eve.id;
-    li.classList="list-group-item d-flex justify-content-between align-items-center";
-    li.innerHTML=` <strong>${eve.name}</strong> 
-                             <strong>${eve.dob}</strong>
-                            <div>  
-                                <i onclick="OnEdit(this)" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
-                                <i onclick="OnDlt(this)"class="fa-solid fa-trash fa-2x fa-2x text-danger"></i> 
-                       
-                             </div>
-    `
-    list.append(li);
-    Swal.fire({
-    title:"New List  Successfully!!",
-    text:"added",
+    li.id=obj.id;
+    li.className="card mb-3 p-3" 
+    li.innerHTML=` 
+    <div>
+                    <strong >${obj.Task}</strong>
+                    </div>
+                    <span>${obj.date}</span>
+                    <p>${obj.topic}</p>
+                    <div class="d-flex justify-content-between">
+                    <i onclick ="OnEdt(this)" class="fa-sharp fa-solid fa-pen-to-square fa-2x text-primary"></i>
+                    <i onclick ="OnRemove(this)" class="fa-sharp fa-solid fa-trash fa-2x text-danger"></i>
+                    </div>
+                    
+        `
+        list.append(li);
+        Swal.fire({
+    title:"Craeted Successfully!!",
     timer:2000,
     icon:"success"
   });
@@ -59,85 +64,91 @@ function createLI(eve){
 }
 
 
-//Remove
-function OnDlt(event){
-let Confirmit=confirm("Are You Sure To Delete")
-if(Confirmit){
-let Remove_Id=event.closest("li").id;
-let Index= array1.findIndex(e=>e.id===Remove_Id);
-array1.splice(Index,1);
-event.closest("li").remove();
-    localStorage.setItem("array1",JSON.stringify(array1));
+//Delete
+function OnRemove(p){
+    let confirmation=confirm("Are u sure to delete")
+    if(confirmation){
+    let RemoveId=p.closest("li").id;
+    let index=WorkArr.findIndex(t=>t.id===RemoveId);
+    WorkArr.splice(index,1);
+    p.closest("li").remove();
+    localStorage.setItem("WorkArr",JSON.stringify(WorkArr));
 Swal.fire({
-    title:"List Deleted Successfully!!",
+    title:"Deleted Successfully!!",
     timer:2000,
     icon:"success"
   });
 
-}
+
+    }
+    
 
 }
 
 //Edit
-function OnEdit(eve){
-    let Edit_id=eve.closest("li").id;
-    let Edobj= array1.find(e=>e.id===Edit_id);
-    formInput1.value=Edobj.name,
-    formInput2.value=Edobj.dob
-    submitButton.classList.add("d-none");
-    updateButton.classList.remove("d-none");
-        localStorage.setItem("Edit_id",Edit_id);
+function OnEdt(el){
+  let editId= el.closest("li").id;
+  let editOBJ=WorkArr.find(t=>t.id===editId);
+  Study.value=editOBJ.Task,
+  Topic.value=editOBJ.topic,
+  Date.value=editOBJ.date,
+  SubmitBtn.classList.add("d-none");
+    UpdateBtn.classList.remove("d-none");
+  localStorage.setItem("editId",editId);
 
 
 }
 
-
 //update
-function onUpdate(pa){
-    let Update_Id=localStorage.getItem("Edit_id");
-    let index=array1.findIndex(e=>e.id===Update_Id);
-    array1[index]={
-        id:Update_Id,
-        name:formInput1.value,
-        dob:formInput2.value
-   
-
-    }
-    let li=document.getElementById(Update_Id)
-    li.querySelectorAll("strong")[0].innerHTML=formInput1.value
-    li.querySelectorAll("strong")[1].innerHTML=formInput2.value
-
-        submitButton.classList.remove("d-none");
-    updateButton.classList.add("d-none");
-      localStorage.setItem("array1",JSON.stringify(array1));
-      form.reset();
-
-    Swal.fire({
+function Onuphandler(el){
+  let updateID= localStorage.getItem("editId");
+  let updatedobj={
+    id:updateID,
+        Task:Study.value,
+        topic:Topic.value,
+        date:Date.value
+  }
+  let index=WorkArr.findIndex(e=>e.id===updateID);
+  WorkArr[index]=updatedobj;
+  localStorage.setItem("WorkArr",JSON.stringify(WorkArr));
+  let li=document.getElementById(updateID)
+  li.querySelectorAll("strong")[0].innerHTML = Study.value;
+li.querySelectorAll("span")[0].innerHTML = Date.value;
+li.querySelectorAll("p")[0].innerHTML = Topic.value;
+SubmitBtn.classList.remove("d-none");
+    UpdateBtn.classList.add("d-none");
+        localStorage.removeItem("editId");
+  form.reset();
+Swal.fire({
     title:"updated Successfully!!",
     timer:2000,
     icon:"success"
   });
 
 
-
-    
 }
 
-function OnClickhandler(e){
-    e.preventDefault();
+
+
+
+
+
+
+function Onhandler(eve){
+    eve.preventDefault();
     let obj={
         id:crypto.randomUUID(),
-        name:formInput1.value,
-        dob:formInput2.value
+        Task:Study.value,
+        topic:Topic.value,
+        date:Date.value
     }
-    array1.push(obj);
+    WorkArr.push(obj);
+    localStorage.setItem("WorkArr",JSON.stringify(WorkArr));
     createLI(obj);
-    localStorage.setItem("array1",JSON.stringify(array1));
     form.reset();
 }
 
 
 
-
-form.addEventListener("submit",OnClickhandler);
-updateButton.addEventListener("click",onUpdate);
+form.addEventListener("submit",Onhandler);
+UpdateBtn.addEventListener("click",Onuphandler);
